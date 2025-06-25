546f2fe7b77432b407f4b6086b4a46e1a9004d76
Dương Trung Nghĩa <dtnghia22@clc.fitus.edu.vn>
2025-06-25 14:05:40 +0700

    Update casia_dataset.py

--- casia_dataset.py
+++ casia_dataset.py
@@ -8,27 +8,34 @@ import pandas as pd
 import numpy as np
 
 class CasiaDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.data = pd.read_csv(csv_file)
+    def __init__(self, root_dir, transform=None):
+        self.samples = []
         self.root_dir = root_dir
         self.transform = transform or transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize((120, 120)),
             transforms.ToTensor()
         ])
+        # Duyệt folder real và spoof
+        for label_name, label in [('real', 0), ('spoof', 1)]:
+            folder = os.path.join(root_dir, label_name)
+            if not os.path.isdir(folder):
+                continue
+            for fname in os.listdir(folder):
+                img_path = os.path.join(folder, fname)
+                if os.path.isfile(img_path):
+                    self.samples.append((img_path, label))
 
     def __len__(self):
-        return len(self.data)
+        return len(self.samples)
 
     def __getitem__(self, idx):
-        row = self.data.iloc[idx]
-        img_path = os.path.join(self.root_dir, row['video_path'])
+        img_path, label = self.samples[idx]
         img = cv2.imread(img_path)
         if img is None:
             img = np.zeros((120, 120, 3), dtype=np.uint8)
         else:
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        label = 1 if row['label'] != 'real' else 0  # spoof:1, real:0
         img = self.transform(img)
         return img, label
 
