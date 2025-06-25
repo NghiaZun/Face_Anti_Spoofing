62e7018d4154e92b3987f105ae7c45a1a53b99e6
Dương Trung Nghĩa <dtnghia22@clc.fitus.edu.vn>
2025-06-25 13:54:28 +0700

    Update train.py

--- train.py
+++ train.py
@@ -4,22 +4,18 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from casia_dataset import CasiaDataset, BalancedSampler
 from resnet15 import ResNet15
-import pandas as pd
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-csv_file = '/kaggle/input/face-check/data/labels.csv'
-root_dir = '/kaggle/input/face-check/data'
+root_dir = '/kaggle/input/face-check/LCC_FASD/LCC_FASD_training'  # Thư mục chứa 2 folder real và spoof
 
-df = pd.read_csv(csv_file)
-labels = [1 if l != 'real' else 0 for l in df['label']]
-dataset = CasiaDataset(csv_file, root_dir)
+dataset = CasiaDataset(root_dir)
+labels = [label for _, label in dataset.samples]
 sampler = BalancedSampler(labels, batch_size=64)
 loader = DataLoader(dataset, batch_size=64, sampler=sampler)
 
 model = ResNet15().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay=5e-4, momentum=0.9)
-
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20], gamma=0.1)
 
 for epoch in range(30):
